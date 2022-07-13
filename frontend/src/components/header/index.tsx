77036ae9af2115c1de2b3aa27e64086f1825579d
Matheus Alves <matalves12345@gmail.com>
2022-07-13 16:21:44 -0300

    Other components

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://github.com/matheus-a-r">@matheus-a-r</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
